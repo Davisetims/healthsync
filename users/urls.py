ac1698a5b4d863a6a1153d1a0175dbcc00d70c2f
Davisetims <etimsintegration@gmail.com>
2025-03-31 21:57:22 +0300

    `Added login functionality and updated templates and views accordingly`

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import index
+from users.views import index, login_view
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login_view, name='login'),
    
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='/login/')  
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")  
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "login.html")
+
+def signup_page_view(request):
+    return render(request, 'signup.html')
+
+
+def logout_view(request):
+    logout(request)  
+    return redirect('/')
+
